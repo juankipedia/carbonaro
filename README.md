9825f3430fabedc8072a6b99c35fbe4bd9d7ac20
juan <juandiegp17@gmail.com>
2021-07-12 21:42:09 -0400

    adding modules for carbonaro compiler

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # carbonaro
+Carbonaro is an small compiler built with the purpose of learning how compilers work.
+
+It supports:
+
+* Numerical variables
+* Basic arithmetic
+* If statements
+* While loops
+* Print text and numbers
+* Input numbers
+* Labels and goto
+* Comments
+
+Take a look at docs to see an example code and language grammar. 
---
+++ carbonaro.py
@@ -0,0 +1,23 @@
+from modules.emit import *
+from modules.lex import *
+from modules.parse import *
+import sys
+
+def main():
+    print("Carbonaro Compiler")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        input = inputFile.read()
+
+    # Initialize the lexer, emitter, and parser.
+    lexer = Lexer(input)
+    emitter = Emitter("out.c")
+    parser = Parser(lexer, emitter)
+
+    parser.program() # Start the parser.
+    emitter.writeFile() # Write the output to file.
+    print("Compiling completed.")
+
+main()
